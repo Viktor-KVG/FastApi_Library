eb595a9e2215cb711992c5e3434b42f8dbba5b62
Viktor-KVG <viktor.kozlov.1991@inbox.ru>
2025-01-19 00:18:17 +0300

    adding endpoints for working with users and administrators

--- src/routers/general_user.py
+++ src/routers/general_user.py
@@ -3,27 +3,32 @@
 Например: routers/user.py, routers/board.py и т.д.
 """
 
-from typing import Any, List, Optional
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import UJSONResponse
-from sqlalchemy import select
-from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, Query,  status, HTTPException
 
-import core.core_user
-from src.database import session_factory, get_db
+import logging
+from typing import List
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from fastapi_jwt import JwtAccessBearer
+from src.settings import SECRET_KEY
+
+from src.auth.auth_jwt import get_current_user, authenticate_user
+from src.models import UserModel
+from src.database import get_db
 from src.schemas import (
+    SearchUsersList,
     Token,
-    UserId,
-    UserLogin,
     UserCreate,
     UserCreateResponse,
     UserForAdmin,
+    UserId,
     UserList,
+    UserLogin,
     UserUpdate
 )
-import core
-from src.auth import auth_jwt
+import core.core_user
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 api_router = APIRouter(
@@ -32,33 +37,106 @@ api_router = APIRouter(
 )
 
 
-@api_router.post("/user", response_model=UserCreateResponse)   
-def create_user(data: UserCreate):
+oauth2_scheme = JwtAccessBearer(secret_key=SECRET_KEY, auto_error=True)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Защищенный эндпоинт
+
+@api_router.get("/protected-endpoint")
+def protected_endpoint(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    logger.info("Received token: %s", token)
+    user = get_current_user(token, db)
+    if user is None:
+        logger.warning("Unauthorized access attempt with token: %s", token)
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    # Используйте точечную нотацию для доступа к атрибутам пользователя
+    logger.info("User  %s is authorized", user.login)  # Исправлено на user.login
+
+    # Убедитесь, что вы возвращаете объект TokenData
+    response_message = f"You are authorized as {user.login}"
+
+    return response_message  # Возвращаем строку
+
+
+@api_router.post("/user", response_model=UserCreateResponse)
+def create_user(data: UserCreate, db: Session = Depends(get_db)):
     if core.core_user.is_user_exist(data):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Error in data entry, such user already exists'
+            detail='User  already exists'
         )
-    else:
-        user = core.register_user(data)
-        return user
+    return core.core_user.register_user(data)
 
-
+# Эндпоинт для аутентификации пользователя
 @api_router.post("/user/login_jwt", response_model=Token)
-def user_login_jwt(data: UserLogin):
-    func = auth_jwt.user_login(data)
-    if func:
-        return {'token': func}
-    elif func == False:
+def user_login_jwt(data: UserLogin, db: Session = Depends(get_db)):
+    auth_result = authenticate_user(db, data.login, data.password)   
+    if auth_result:
+        return {'token': auth_result['token']} 
+      
+    logger.warning("Неверные учетные данные для пользователя: %s", data.login)
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail='Authentication error, incorrect credentials'
+    )
+
+
+@api_router.put("/user/me", response_model=UserForAdmin)
+def update_user(data: UserUpdate, db: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)):
+    user = core.core_user.update_user_data(db, current_user, data)
+    return UserForAdmin.from_orm(user)
+
+
+@api_router.put('/user/{user_id}', response_model=UserForAdmin)
+def put_user_id(user_id: int, user_update: UserUpdate, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    user = get_current_user(token, db)
+    if user is None:
+        logger.warning("Unauthorized access attempt with token: %s", token)
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    if user_id <= 0:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail='Internal Server Error'
-            )
-    else:
+            status_code=status.HTTP_417_EXPECTATION_FAILED,
+            detail='User ID must not be zero'
+        )    
+    user_put = core.core_user.search_user_by_id_put(user_update, user_id, db)
+    if user_put:
+        return user_put   
+    if user_put == False:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Authentication error, incorrect credentials'
+            detail='Invalid user '
         )
+
+
+@api_router.delete('/user/{user_id}')
+def delete_user_id(user_id: int, db: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Not authorized to access this resource')
+    if user_id <= 0:
+        raise HTTPException(
+            status_code=status.HTTP_417_EXPECTATION_FAILED,
+            detail='User ID must not be zero'
+        )    
+    user_del = core.core_user.search_user_by_id_for_delete(UserId(id=user_id), db)
+    if user_del:
+        return user_del    
+    if user_del == False:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Invalid user '
+        )  
+
+
+# Эндпоинт для получения списка читателей (только для администраторов)
+@api_router.get("/user/list", response_model=List[UserList])
+def list_readers(user_id: int = None, user_login: str = None, user_email: str = None, 
+                 db: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Not authorized to access this resource')   
+    result_list = core.core_user.search_list_users(SearchUsersList(id=user_id, login=user_login, email=user_email), db)
+    return result_list
 
 
 
