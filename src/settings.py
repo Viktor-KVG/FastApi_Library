de5bb05e0a82c5a97535025788a51202b51428a1
Viktor-KVG <viktor.kozlov.1991@inbox.ru>
2025-01-19 00:21:03 +0300

    updating values

--- src/settings.py
+++ src/settings.py
@@ -8,6 +8,6 @@ DB_NAME = "postgres_library"
 DB_SESSION_ECHO = True # Используется при создании engine
 
 
-SECRET_KEY = "VX0KvQRUTNOJRJJyAVntIsTUiJ+h4uvg+lcpJtB7elI="
+SECRET_KEY = "29c1b96c17b6881edf2e32d85e7ff6ce88b9b5ba6facba5cff2fb10a0082630d"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
