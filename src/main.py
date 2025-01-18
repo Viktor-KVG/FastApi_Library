7d3e61a3acafc2e00b9741b3c49f29b8fdf1a31f
Viktor-KVG <viktor.kozlov.1991@inbox.ru>
2025-01-19 00:14:59 +0300

    adding routers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from src.routers import general_user
+
+from src.routers import general_user, general_author
 
 app = FastAPI()
 
 app.include_router(general_user.api_router)
+app.include_router(general_author.api_router)
