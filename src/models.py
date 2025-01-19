a3c540583b00fb9dfc9e935e74a95afacab142a2
Viktor-KVG <viktor.kozlov.1991@inbox.ru>
2025-01-19 23:13:13 +0300

    Editing models

--- src/models.py
+++ src/models.py
@@ -27,7 +27,6 @@ class UserModel(Base):
     updated_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
     is_admin: Mapped[bool] = mapped_column(default=False, nullable=False)
     
-    # Изменено на правильное имя для обратной связи
     books: Mapped[List["BookModel"]] = relationship("BookModel", back_populates="user", uselist=True)
 
 class BookModel(Base):
@@ -38,13 +37,16 @@ class BookModel(Base):
     description: Mapped[str] = mapped_column(String(500))
     created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), default=func.now(), nullable=False)
     author_id: Mapped[int] = mapped_column(ForeignKey('author.id'))
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))  # Исправлено на 'user.id'
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id')) 
     genre: Mapped[str] = mapped_column(String(120))
     quantity: Mapped[int] = mapped_column(nullable=True)
-    
-    # Исправлено на правильное имя для обратной связи
+
     user: Mapped["UserModel"] = relationship("UserModel", back_populates="books")
     author: Mapped["AuthorModel"] = relationship("AuthorModel", back_populates="books")
+    @property
+    def creator_id(self):
+        return self.user_id  # Возвращает user_id как creator_id
+    
 
 class AuthorModel(Base):
     __tablename__ = 'author'
@@ -53,8 +55,7 @@ class AuthorModel(Base):
     name: Mapped[str] = mapped_column(String(120))
     biography: Mapped[str] = mapped_column(String(800))
     date_of_birth: Mapped[Date] = mapped_column(Date)
-    
-    # Исправлено на правильное имя для обратной связи
+
     books: Mapped[List["BookModel"]] = relationship("BookModel", back_populates="author", uselist=True)
 
 
