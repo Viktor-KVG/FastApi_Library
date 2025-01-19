3d3be9ebdf18a3a5c9896af374b372b3dcda7dac
Viktor-KVG <viktor.kozlov.1991@inbox.ru>
2025-01-19 23:16:37 +0300

    editing user endpoints

--- src/routers/general_user.py
+++ src/routers/general_user.py
@@ -41,8 +41,8 @@ oauth2_scheme = JwtAccessBearer(secret_key=SECRET_KEY, auto_error=True)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Защищенный эндпоинт
 
+# Защищенный эндпоинт для тестирования прав доступа
 @api_router.get("/protected-endpoint")
 def protected_endpoint(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     logger.info("Received token: %s", token)
@@ -50,16 +50,12 @@ def protected_endpoint(token: str = Depends(oauth2_scheme), db: Session = Depend
     if user is None:
         logger.warning("Unauthorized access attempt with token: %s", token)
         raise HTTPException(status_code=401, detail="Unauthorized")
-
-    # Используйте точечную нотацию для доступа к атрибутам пользователя
-    logger.info("User  %s is authorized", user.login)  # Исправлено на user.login
-
-    # Убедитесь, что вы возвращаете объект TokenData
+    logger.info("User  %s is authorized", user.login)
     response_message = f"You are authorized as {user.login}"
+    return response_message
 
-    return response_message  # Возвращаем строку
 
-
+# Эндпоинт для создания пользователя
 @api_router.post("/user", response_model=UserCreateResponse)
 def create_user(data: UserCreate, db: Session = Depends(get_db)):
     if core.core_user.is_user_exist(data):
@@ -68,6 +64,7 @@ def create_user(data: UserCreate, db: Session = Depends(get_db)):
             detail='User  already exists'
         )
     return core.core_user.register_user(data)
+
 
 # Эндпоинт для аутентификации пользователя
 @api_router.post("/user/login_jwt", response_model=Token)
@@ -83,18 +80,18 @@ def user_login_jwt(data: UserLogin, db: Session = Depends(get_db)):
     )
 
 
+# Эндпоинт для обновления аутентифицированного пользователя
 @api_router.put("/user/me", response_model=UserForAdmin)
 def update_user(data: UserUpdate, db: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)):
     user = core.core_user.update_user_data(db, current_user, data)
     return UserForAdmin.from_orm(user)
 
 
+# Эндпоинт для редактирования пользователей(только для администраторов)
 @api_router.put('/user/{user_id}', response_model=UserForAdmin)
-def put_user_id(user_id: int, user_update: UserUpdate, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    user = get_current_user(token, db)
-    if user is None:
-        logger.warning("Unauthorized access attempt with token: %s", token)
-        raise HTTPException(status_code=401, detail="Unauthorized")
+def put_user_id(user_id: int, user_update: UserUpdate, current_user: UserModel = Depends(get_current_user), db: Session = Depends(get_db)):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Not authorized to access this resource')
     if user_id <= 0:
         raise HTTPException(
             status_code=status.HTTP_417_EXPECTATION_FAILED,
@@ -109,7 +106,7 @@ def put_user_id(user_id: int, user_update: UserUpdate, token: str = Depends(oaut
             detail='Invalid user '
         )
 
-
+# Эндпоинт для удаления пользователя (только для администраторов)
 @api_router.delete('/user/{user_id}')
 def delete_user_id(user_id: int, db: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)):
     if not current_user.is_admin:
