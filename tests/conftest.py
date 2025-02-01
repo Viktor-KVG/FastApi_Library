44fadd11254f31351327d49d291ad2dacb431d94
Viktor-KVG <viktor.kozlov.1991@inbox.ru>
2025-02-01 17:54:50 +0300

    added fixtures for test functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,42 @@
 '''Фикстуры для тестирования функций, связанных с пользователями, включая создание, обновление и поиск пользователей.
-   Используются мок-объекты для имитации взаимодействия с базой данных и создания пользователей.'''
+   Используются мок-объекты для имитации взаимодействия с базой данных.'''
 
+import jwt
 import pytest
 from fastapi.testclient import TestClient
 from unittest.mock import MagicMock
-from schemas import SearchUsersList, UserCreate, UserId, UserUpdate
+from settings import ALGORITHM, SECRET_KEY
+from src.database import get_db
+from schemas import  CreateBookModel, SearchUsersList, UserCreate, UserId, UserUpdate
 from src.main import app 
 from src.models import UserModel
 from sqlalchemy.orm import Session
+import logging
+
 
 client = TestClient(app)
 
+mock_session_db = MagicMock()
 
-@pytest.fixture
-def mock_db():
-    return MagicMock(Session)
+def override_get_db():
+    try:
+        yield mock_session_db
+    finally:
+        pass   
 
+app.dependency_overrides[get_db] = override_get_db     
+
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @pytest.fixture
 def mock_session():
+    logger.info("Создание мока для сессии базы данных.")
     session = MagicMock(spec=Session)
     yield session
+    logger.info("Завершение работы мока для сессии базы данных.")
 
 
 @pytest.fixture
@@ -39,12 +55,50 @@ def mock_create_user():
 
 @pytest.fixture
 def mock_user_create():
-    return UserCreate(login="testuser", password="password", email="test@example.com")
+    return UserCreate(login="test_user",
+    password="test_password",
+    email="test@example.com",
+    is_admin=False)
+
+
+@pytest.fixture
+def mock_book_create():
+    return CreateBookModel(
+            title="test title",
+            description="test description",
+            author_id= 1,
+            genre='genre',
+            quantity= 1,
+            )
+
+
+@pytest.fixture
+def mock_book_update():
+    return CreateBookModel(
+            title="test update title",
+            description="test update description",
+            author_id= 1,
+            genre='triller',
+            quantity= 1,
+            )
+
+
+@pytest.fixture
+def admin_jwt_token():
+    """Фикстура для генерации JWT-токена для администратора."""
+    payload = {
+        "subject": "test_admin_user",
+        "is_admin": True
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 
 @pytest.fixture
 def mock_user_update():
-    return UserUpdate(login="updateduser", email="updated@example.com", password="newpassword", is_admin=False)
+    return UserUpdate(login="updateduser", 
+                      email="updated@example.com", 
+                      password="newpassword", 
+                      is_admin=False)
 
 
 @pytest.fixture
@@ -54,13 +108,11 @@ def mock_user_id():
 
 @pytest.fixture
 def mock_search_users_list():
-    return SearchUsersList(id=None, login=None, email=None, limit=10, offset=0)
-
-
-@pytest.fixture
-def mock_user_model():
-    user = UserModel(id=1, login="testuser", email="test@example.com", password_hash="hashed_password", is_admin=False)
-    return user
+    return SearchUsersList(id=None, 
+                           login=None, 
+                           email=None, 
+                           limit=10, 
+                           offset=0)
 
 
 @pytest.fixture
