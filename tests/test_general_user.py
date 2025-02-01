56f482c0e3cb97c09678550101911345cd3a0470
Viktor-KVG <viktor.kozlov.1991@inbox.ru>
2025-02-01 17:59:04 +0300

    added test functions for user operations

--- tests/test_general_user.py
+++ tests/test_general_user.py
@@ -1,7 +1,10 @@
 '''Тесты для функций, связанных с пользователями, включая проверку существования пользователя, регистрацию, обновление данных и удаление.
    Используются мок-объекты для имитации взаимодействия с базой данных и создания JWT токенов.'''
 
-from unittest.mock import MagicMock
+from datetime import date
+from unittest.mock import MagicMock, patch
+from fastapi import HTTPException
+from conftest import client
 import logging
 from core.core_user import (
     register_user,
@@ -15,8 +18,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def test_register_user(mock_create_user, mock_user_create):
-
+def test_register_user_with_token(mock_create_user, mock_user_create):
     mock_create_user, mock_user = mock_create_user
 
     # Мокаем создание JWT токена
@@ -35,7 +37,6 @@ def test_register_user(mock_create_user, mock_user_create):
     logger.debug("Тест успешно пройден")
 
 
- 
 def test_search_user_by_id_put(mock_session, mock_user_update, mock_user_model):
     # Настраиваем мок-сессию так, чтобы она возвращала пользователя с другим логином
     mock_session.query.return_value.filter.return_value.first.side_effect = [mock_user_model, None]   
@@ -72,7 +73,114 @@ def test_search_user_by_id_for_delete(mock_session, mock_user_id, mock_user_mode
     mock_session.commit.assert_called_once()
 
    
+def test_user_register(mock_user_create, admin_jwt_token):
+    # Мок для функции is_user_exist (пользователь не существует)
+    with patch("core.core_user.is_user_exist", return_value=False):
+        # Мок для функции register_user
+        with patch("core.core_user.register_user", return_value={
+            "id": 1,
+            "login": "test_user1",
+            "email": "test@example.com",
+            "is_admin": True,
+            "token": admin_jwt_token
+            }):
+            # Мок для create_access_token
+            with patch("src.auth.auth_jwt.create_access_token", return_value=admin_jwt_token):
+                # Создание пользователя
+                response_create = client.post("/api/user/", json=mock_user_create.dict())
+                print(response_create.json())
+                assert response_create.status_code == 200
+                assert response_create.json() == {
+                    "id": 1,
+                    "login": "test_user1",
+                    "email": "test@example.com",
+                    "is_admin": True,
+                    "token": admin_jwt_token
+                    }
 
 
+def test_create_user_with_existing_login():
+    # Предполагаем, что эндпоинт должен возвращать HTTPException с соответствующим сообщением
+    user_data = {
+        "login": "ExistingUser ",
+        "password": "password",
+        "email": "existing@example.com",
+        "is_admin": False
+    }
 
+    # Мокаем функцию так, чтобы она выбрасывала HTTPException
+    with patch("core.core_user.register_user", side_effect=HTTPException(status_code=400, 
+                                                                         detail="User with this login already exists.")):
+        # Вызываем эндпоинт создания пользователя
+        response = client.post("/api/user/", json=user_data)
+
+        # Проверяем, что возвращается ошибка
+        assert response.status_code == 400
+        assert response.json() == {"detail": "User with this login already exists."}
    
+
+def test_user_login_invalid_password():
+    # Мокируем функцию authenticate_user
+    with patch('src.auth.auth_jwt.authenticate_user', return_value=None):
+        response = client.post("/api/user/login_jwt/", json={"login": "TestUser_1", "password": "wrong_password"})
+
+        # Проверяем статус код и содержимое ответа
+        assert response.status_code == 400  # Неавторизован
+        assert 'token' not in response.json() 
+
+
+def test_user_login_user_not_found():
+    # Мокируем функцию authenticate_user
+    with patch('src.auth.auth_jwt.authenticate_user', return_value=None):
+        response = client.post("/api/user/login_jwt/", json={"login": "TestUser_2", "password": "password"})
+
+        # Проверяем статус код и содержимое ответа
+        assert response.status_code == 400  # Неавторизован
+        assert 'token' not in response.json()          
+
+
+def test_login_invalid_credentials():
+    response = client.post("/api/user/login_jwt/", json={"login": "wronguser", "password": "wrongpassword"})
+    
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Authentication error, incorrect credentials"}        
+
+
+def test_user_update(mock_user_update, admin_jwt_token):
+    # Мок для функции is_user_exist (пользователь не существует)
+    with patch("core.core_user.is_user_exist", return_value=False):
+        # Мок для функции register_user
+        with patch("core.core_user.search_user_by_id_put", return_value={
+            "id": 1,
+            "login": "updateduser",
+            "email": "updated@example.com",
+            "created_at": date(2025, 1, 31).isoformat(),
+            "updated_at": date(2025, 1, 31).isoformat(),
+            "is_admin": False,
+            "books": []
+            }):
+            response_create = client.put("/api/user/1/", json=mock_user_update.dict(), 
+                                         headers={"Authorization": f"Bearer {admin_jwt_token}"})
+            print(response_create.json())
+            assert response_create.status_code == 200
+            assert response_create.json() == {
+                "id": 1,
+                "login": "updateduser",
+                "email": "updated@example.com",
+                "created_at": "2025-01-31T00:00:00",
+                "updated_at": "2025-01-31T00:00:00",
+                "is_admin": False,
+                "books": []
+                }
+
+
+def test_user_delete(admin_jwt_token):
+    # Мок для функции is_user_exist (пользователь не существует)
+    with patch("core.core_user.is_user_exist", return_value=False):
+        # Мок для функции register_user
+        with patch("core.core_user.search_user_by_id_for_delete", return_value={'details': 'User deleted'}):
+
+            response_delete = client.delete(f"/api/user/{1}", headers={"Authorization": f"Bearer {admin_jwt_token}"})               
+            # Проверка статуса ответа
+            assert response_delete.status_code == 200
+            assert response_delete.json() == {'details': 'User deleted'}   
